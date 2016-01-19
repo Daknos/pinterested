4a7eb817fc341d832a640cbc99856a34ac09756c
dakno <leonardbibby@hotmail.com>
2016-01-19 20:47:39 +0000

    Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = {host: ' pinterested99.heroukapp.com' }
+  config.action_mailer.default_url_options = {host: 'pinterestedover9000.herokuapp.com' }
 end
